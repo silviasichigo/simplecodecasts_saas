90df7d490eb5c011dcb3de55e2b694332a6683ef
Michael Tyler <michael.w.tyler@gmail.com>
2016-05-18 10:10:11 +0000

    Added contacts table and model

---
+++ db/migrate/20160518100304_creat_contacts.rb
@@ -0,0 +1,11 @@
+class CreatContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :comments
+      
+      t.timestamps
+    end
+  end
+end
