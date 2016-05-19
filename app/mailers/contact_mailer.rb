a6e3416109d8a6850c581086a80af84cd8f12385
Michael Tyler <michael.w.tyler@gmail.com>
2016-05-19 09:00:21 +0000

    corrected my mail address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'your@email_address.com'
+  default to: 'mtyler@fastmail.fm'
   def contact_email(name, email, body)
     @name = name
     @email = email
